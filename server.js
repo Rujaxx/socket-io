44a2a21e37f3120212ec9ed7abf96ae98a99082e
Rujaxx <shaikhrujan@gmail.com>
2021-05-29 14:19:50 +0530

    completed basic chat app using socket.io

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app)
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
 
 server.listen(4000,()=>{console.log('Server started on 4000')})
